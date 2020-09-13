c12efea9f938fcf36db7db267f6d0bd6a00a3593
Paolo <55134717+paolobeakovic@users.noreply.github.com>
2020-09-13 23:11:01 +0200

    Add files via upload

---
+++ beakovicPaoloPonavljanje2/beakovicPaoloPonavljanje2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beakovicPaoloPonavljanje2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sekunde;
+            int minute;
+            int sek;
+
+
+            Console.WriteLine("Upiši sekunde");
+            sekunde = Convert.ToInt32(Console.ReadLine());
+            minute = sekunde / 60;
+            sek = sekunde % 60;
+            Console.WriteLine("{0} minuta i {1} sekundi", minute, sek);
+            Console.ReadKey();
+
+
+        }
+    }
+}
